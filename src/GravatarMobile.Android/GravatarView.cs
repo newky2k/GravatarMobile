cf2b5c24c0c22206daf16664f2f1ebd5a54e14f8
Dave <newky2k@mac.com>
2014-06-01 23:16:01 +0100

    Work on the android sample
    Work on the android sample

---
+++ src/GravatarMobile.Android/GravatarView.cs
@@ -0,0 +1,70 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using GravatarMobile.Core.Interfaces;
+using GravatarMobile.Core;
+
+namespace GravatarMobile.Android
+{
+	/// <summary>
+	/// Android Gravatar View
+	/// </summary>
+	public class GravatarView : ImageView, IGravatarView
+	{
+		#region Fields
+
+		private Gravatar mItem;
+
+		#endregion
+		#region Properties
+		public Gravatar Avatar
+		{
+			get
+			{
+				return mItem;
+			}
+			set
+			{
+				mItem = value;
+			}
+		}
+		#endregion
+
+		#region Constructors
+		public GravatarView(Context context) :
+			base(context)
+		{
+			Initialize();
+		}
+
+		public GravatarView(Context context, IAttributeSet attrs) :
+			base(context, attrs)
+		{
+			Initialize();
+		}
+
+		public GravatarView(Context context, IAttributeSet attrs, int defStyle) :
+			base(context, attrs, defStyle)
+		{
+			Initialize();
+		}
+
+		void Initialize()
+		{
+
+		}
+
+		#endregion
+	}
+}
+
