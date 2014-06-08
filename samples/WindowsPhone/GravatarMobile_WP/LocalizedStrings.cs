667fe63cd6c75ef5e5106d033640fecb0a3a3168
Dave <david.humphreys@xamarin.com>
2014-06-08 22:11:06 +0100

    Started Window Phone port
    Started Window Phone port

---
+++ samples/WindowsPhone/GravatarMobile_WP/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using GravatarMobile_WP.Resources;
+
+namespace GravatarMobile_WP
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
